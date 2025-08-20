f38391fbbcea686d921e2cbb54998b6fef43474a
Vladimir Denisov <rygyj95@gmail.com>
2025-08-20 10:01:13 +0500

    styles: fix styles of share button

--- src/components/ShareButton.vue
+++ src/components/ShareButton.vue
@@ -10,49 +10,40 @@ function toggleShare() {
 </script>
 
 <template>
-    <button 
+  <button 
       class="share-button" 
       :class="{ 'share-button--active': isActive }"
       @click="toggleShare"
     >
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle class="share-button__circle" cx="16" cy="16" r="16" fill="currentColor"/>
-    <path class="share-button__arrow" d="M24 15.495L17.7663 9.01389V12.8806H16.4409C12.3314 12.8806 9 16.0393 9 19.9358V21.9849L9.58868 21.3733C11.5901 19.2941 14.4221 18.1094 17.3912 18.1094H17.7663V21.9761L24 15.495Z" fill="currentColor"/>
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="15" height="13"><path class="share-button__icon" fill="currentColor" d="M15 6.495L8.766.014V3.88H7.441C3.33 3.88 0 7.039 0 10.936v2.049l.589-.612C2.59 10.294 5.422 9.11 8.39 9.11h.375v3.867L15 6.495z"/></svg>
   </button>
 </template>
 
 <style scoped>
-.share-button {
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
-  cursor: pointer;
-}
+  .share-button {
+    display: flex;
+    width: 32px;
+    height: 32px;
+    align-items: center;
+    background-color: var(--gray-200);
+    border-radius: 50%;
+    cursor: pointer;
+    justify-content: center;
+  }
 
-.share-button__circle {
-  fill: var(--gray-200);
-}
+  .share-button__icon {
+    fill: var(--gray-500);
+  }
 
-.share-button:hover .share-button__circle,
-.share-button:focus-visible .share-button__circle {
-  fill: var(--gray-500);
-}
+  .share-button:hover,
+  .share-button:focus-visible,
+  .share-button--active {
+    background-color: var(--gray-500);
+  }
 
-.share-button__arrow {
-  fill: var(--gray-500);
-}
-
-.share-button:hover .share-button__arrow,
-.share-button:focus-visible .share-button__arrow {
-  fill: var(--gray-200);
-}
-
-.share-button--active .share-button__circle {
-  fill: var(--gray-500);
-}
-
-.share-button--active .share-button__arrow {
-  fill: var(--gray-200);
-}
+  .share-button:hover .share-button__icon,
+  .share-button:focus-visible .share-button__icon,
+  .share-button--active .share-button__icon {
+    fill: var(--gray-200);
+  }
 </style>
